8a55dab5f27a467f6c6bdfa52f223dab539d6864
Flora-manda <flora.uwadiegwu@gmail.com>
2020-03-17 23:30:31 +0000

    updated server logic with POST request that sets cookie value and template var with username

--- express_server.js
+++ express_server.js
@@ -78,12 +78,12 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
